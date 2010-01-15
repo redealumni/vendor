6d6a0bf21bd692027b01ccd56722bc9915ce0699
Dalton Pinto <efqdalton@gmail.com>
2010-01-14 23:52:22 -0200

    initial commit

---
+++ plugins/markaby/init.rb
@@ -0,0 +1,9 @@
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'lib'))
+
+require 'markaby'
+require 'markaby/rails'
+
+#broken in rails edge
+#ActionView::Base::register_template_handler 'mab', Markaby::Rails::ActionViewTemplateHandler
+
+ActionController::Base.send :include, Markaby::Rails::ActionControllerHelpers
